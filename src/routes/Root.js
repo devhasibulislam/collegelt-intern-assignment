ccef0ecca1885820bd3aa445671174a441f4d863
hasibulislam999 <hasib143sl@gmail.com>
2022-06-09 00:07:27 +0600

    adding time functionality done

--- src/routes/Root.js
+++ src/routes/Root.js
@@ -5,12 +5,14 @@ import './css/root.css';
 
 const Root = () => {
     const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const getUsers = async () => {
             const url = "https://randomuser.me/api/?results=10";
             const { data } = await axios.get(url);
             setUsers(data?.results);
+            setLoading(false);
         };
         getUsers();
     }, []);
@@ -24,31 +26,37 @@ const Root = () => {
     return (
         <div className="table-wrapper">
             <div className="browser-mockup">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Gender</th>
-                            <th>City</th>
-                            <th>State</th>
-                            <th>Country</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            users?.map(user => <tr
-                                key={user?.email}
-                                onClick={() => handleUserProfile(user)}
-                            >
-                                <td data-column="Name">{user?.name?.title} {user?.name?.first} {user?.name?.last}</td>
-                                <td data-column="Gender">{user?.gender}</td>
-                                <td data-column="City">{user?.location?.city}</td>
-                                <td data-column="State">{user?.location?.state}</td>
-                                <td data-column="Country">{user?.location?.country}</td>
-                            </tr>)
-                        }
-                    </tbody>
-                </table>
+                {
+                    loading
+                        ?
+                        <p style={{textAlign: 'center'}}>loading...</p>
+                        :
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Gender</th>
+                                    <th>City</th>
+                                    <th>State</th>
+                                    <th>Country</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    users?.map(user => <tr
+                                        key={user?.email}
+                                        onClick={() => handleUserProfile(user)}
+                                    >
+                                        <td data-column="Name">{user?.name?.title} {user?.name?.first} {user?.name?.last}</td>
+                                        <td data-column="Gender">{user?.gender}</td>
+                                        <td data-column="City">{user?.location?.city}</td>
+                                        <td data-column="State">{user?.location?.state}</td>
+                                        <td data-column="Country">{user?.location?.country}</td>
+                                    </tr>)
+                                }
+                            </tbody>
+                        </table>
+                }
             </div>
         </div>
     );
--- src/routes/User.js
+++ src/routes/User.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import './css/user.css';
 
 const User = () => {
     const { email } = useParams();
     const user = JSON.parse(localStorage.getItem('user'));
+
+    const [timer, setTimer] = useState('');
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const setCountDownTo = new Date(user?.dob?.date);
+        console.log(setCountDownTo);
+        const nextYear = new Date().getFullYear() + 1;
+        console.log(nextYear);
+        if (setCountDownTo.getMonth() <= new Date().getMonth() && setCountDownTo.getDay() <= new Date().getDate()) {
+            setCountDownTo.setFullYear(nextYear);
+        } else {
+            setCountDownTo.setFullYear(nextYear - 1);
+        }
+
+        /* console.log(setCountDownTo.getMonth(), 'user month');
+        console.log(setCountDownTo.getDay(), 'user day');
+        console.log(new Date().getMonth(), 'current month');
+        console.log(new Date().getDate(), 'current day'); */
+
+        const getCountDownTo = setCountDownTo.getTime();
+
+        const counter = setInterval(() => {
+            const getCountDownFrom = new Date().getTime();
+            const distance = getCountDownTo - getCountDownFrom;
+
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            if (distance < 0) {
+                clearInterval(counter);
+                setTimer('Happy Birthday to you!!!');
+            } else {
+                setTimer(`${days} days : ${hours} hours : ${minutes} minutes : ${seconds} seconds`);
+            }
+            setLoading(false);
+        }, 1000);
+    }, [user?.dob?.date]);
 
     return (
         <div
@@ -21,7 +61,8 @@ const User = () => {
                     />
                     <p id='phone-number'>phone: <b>{user?.phone}</b></p>
                     <p id='dob'>date of birth: <b>{user?.dob?.date.split('T')[0]}</b></p>
-                    <p id=''></p>
+                    <p id='timer'>{timer}</p>
+                    {loading && 'loading...'}
                 </div>
             }
         </div>
